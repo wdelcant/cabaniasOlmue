0692cf94290a0b2ffac7a56a70e99e49cc278ce6
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-07-21 02:19:00 -0400

    Se agrega loader

--- js/utils/imgLoader.js
+++ js/utils/imgLoader.js
@@ -2,16 +2,11 @@
 window.addEventListener('load', precargaImagenes);
 
 function precargaImagenes() {
-
     let img = new Image();
     img.src = '/assets/img/bannertop.jpg' || '/assets/img/foto8.jpeg';
 
-
     //Cuando se terminan de cargar las imágenes se le agrega la clase "close" a #wrap-preload.
     let preload = document.getElementById('wrap-preload');
-
     preload.classList.add('close');
-
-    console.log('Sitio cargado!');
 
 }
