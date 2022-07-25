babcbb26d2027cb884911cd6ba372fc46ca7b0ac
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-07-25 17:55:39 -0400

    Se actualizan funciones del login

--- js/login.js
+++ js/login.js
@@ -9,6 +9,11 @@ let password;
 let passwordTwo;
 
 // Inicio del programa
+function main() {
+	loginRegister();
+	initializeEvents();
+	getUsersLocalStorage();
+}
 
 class User {
 	constructor(usernameValue, emailValue, passwordValue, passwordTwoValue) {
@@ -26,8 +31,7 @@ function loginRegister() {
 	password = document.getElementById('password');
 	passwordTwo = document.getElementById('passwordTwo');
 }
-//=========================================FUNCION LOGIN=================================================
-
+//=========================================FUNCIÓN LOGIN=================================================
 
 function initializeEvents() {
 	formLogin.onsubmit = (e) => checkInputs(e);
@@ -46,47 +50,39 @@ function checkInputs(e) {
 		passwordTwoValue);
 	login.push(userToRegister);
 
-
 	// Valida usuario
-
-	if (usernameValue === '') {
-		setErrorFor(username, 'El usuario no puede estar vacío');
-	} else if (usernameValue.length < 3) {
-		setErrorFor(username, 'Debe tener al menos 3 caracteres');
-	} else if (usernameValue.length > 15) {
-		setErrorFor(username, 'Debe tener menos de 15 caracteres');
-	} else {
-		setSuccessFor(username);
+	const validaUsername = () => {
+		usernameValue === '' ? setErrorFor(username, 'El usuario no puede estar vacío') :
+			usernameValue.length < 3 ? setErrorFor(username, 'El usuario debe tener al menos 3 caracteres') :
+			usernameValue.length > 20 ? setErrorFor(username, 'El usuario debe tener menos de 20 caracteres') :
+			setSuccessFor(username);
 	}
+	validaUsername();
 
-	// valida el correo electrónico
-	if (emailValue === '') {
-		setErrorFor(email, 'El correo no puede estar vacío');
-	} else if (!isEmail(emailValue)) {
-		setErrorFor(email, 'El correo no es válido');
-	} else {
-		setSuccessFor(email);
+	// Valida email
+	const validaEmail = () => {
+		emailValue === '' ? setErrorFor(email, 'El email no puede estar vacío') :
+			emailValue = !isEmail(emailValue) ? setErrorFor(email, 'El email no es válido') :
+			setSuccessFor(email);
 	}
+	validaEmail();
 
-	// valida la contraseña
-	if (passwordValue === '') {
-		setErrorFor(password, 'La contraseña no puede estar vacía');
-	} else if (passwordValue.length < 6) {
-		setErrorFor(password, 'Debe tener al menos 6 caracteres');
-	} else if (passwordValue.length > 15) {
-		setErrorFor(password, 'Debe tener menos de 15 caracteres');
-	} else {
-		setSuccessFor(password);
+	// Valida password
+	const validaPassword = () => {
+		passwordValue === '' ? setErrorFor(password, 'La contraseña no puede estar vacía') :
+			passwordValue.length < 6 ? setErrorFor(password, 'La contraseña debe tener al menos 6 caracteres') :
+			passwordValue.length > 20 ? setErrorFor(password, 'La contraseña debe tener menos de 20 caracteres') :
+			setSuccessFor(password);
 	}
+	validaPassword();
 
 	// valida la contraseña coincidente
-	if (passwordTwoValue === '') {
-		setErrorFor(passwordTwo, 'La contraseña no puede estar vacía');
-	} else if (passwordValue !== passwordTwoValue) {
-		setErrorFor(passwordTwo, 'La contraseña no coincide');
-	} else {
-		setSuccessFor(passwordTwo);
+	function validaPasswordTwo() {
+		passwordTwoValue === '' ? setErrorFor(passwordTwo, 'La contraseña no puede estar vacía') :
+			passwordTwoValue !== passwordValue ? setErrorFor(passwordTwo, 'Las contraseñas no coinciden') :
+			setSuccessFor(passwordTwo);
 	}
+	validaPasswordTwo();
 
 	if (usernameValue && emailValue && passwordValue && passwordTwoValue) {
 		passwordTwoValue !== passwordTwoValue ? setErrorFor(passwordTwo, 'La contraseña no coincide') : addUserLocalStorage(), window.location.href = "./pages/reserva.html";
@@ -94,7 +90,6 @@ function checkInputs(e) {
 }
 
 // Agrega los usuarios registrados al localStorage
-
 function addUserLocalStorage() {
 	localStorage.setItem("userList", JSON.stringify(login)); // guarda los usuarios en localStorage
 }
@@ -104,13 +99,7 @@ function getUsersLocalStorage() {
 	storedUsers !== null ? login = JSON.parse(storedUsers) : login = []; // si hay usuarios guardados los guarda en el array login
 }
 
-// Inicio del programa
-
-function main() {
-	loginRegister();
-	initializeEvents();
-	getUsersLocalStorage();
-}
+// llama a inicializar el programa
 main();
 
-//=========================================V.2.0=================================================
+//=========================================V.3.0=================================================
